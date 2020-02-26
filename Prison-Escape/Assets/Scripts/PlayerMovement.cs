733853d3a6f0860b27d2c8d81e2851f5d752b477
XboxPro39 <57122545+XboxPro39@users.noreply.github.com>
2020-02-26 13:50:25 -0600

    Made Player Move

---
+++ Prison-Escape/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour 
+{
+
+	public float speed;
+
+	private Rigidbody2D rb;
+	private Vector2 moveVelocity;
+
+	void Start()
+	{
+
+		rb = this.GetComponent<Rigidbody2D>();
+
+	}
+
+	void Update()
+	{
+
+		Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+		moveVelocity = input.normalized * speed;
+
+	}
+
+	void FixedUpdate()
+	{
+
+		rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+
+	}
+
+}
