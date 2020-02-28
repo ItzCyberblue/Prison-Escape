02448fb9f3acd31085fb193bbf943ba76f215ea8
XboxPro39 <57122545+XboxPro39@users.noreply.github.com>
2020-02-27 22:07:14 -0600

    Added New Stuff and Organized Stuff

---
+++ Prison-Escape/Assets/Scripts/NPCs/Patrol.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour 
+{
+
+	public float speed;
+	private float waitTime;
+	public float startWaitTime;
+
+	public Transform[] positions;
+	private int randPos;
+
+	void Start()
+	{
+
+		waitTime = startWaitTime;
+		randPos = Random.Range(0, positions.Length);
+
+	}
+
+	void Update()
+	{
+
+		transform.position = Vector2.MoveTowards(transform.position, positions[randPos].position, speed * Time.deltaTime);
+
+		if(Vector2.Distance(transform.position, positions[randPos].position) < 0.2f)
+		{
+
+			if(waitTime <= 0)
+			{
+
+				randPos = Random.Range(0, positions.Length);
+				waitTime = startWaitTime;
+
+			}
+			else
+			{
+
+				waitTime -= Time.deltaTime;
+
+			}
+
+		}
+
+	}
+
+}
