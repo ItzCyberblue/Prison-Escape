b8e9cbb0308806806c1b503a1be3d1106d178069
XboxPro39 <57122545+XboxPro39@users.noreply.github.com>
2020-02-28 20:04:03 -0600

    Added lots of stuff

---
+++ Prison-Escape/Assets/Scripts/Sequences/DeathScreen.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathScreen : MonoBehaviour 
+{
+
+	void Update()
+	{
+
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+
+			SceneManager.LoadScene(0);
+
+		}
+
+	}
+
+}
--- Prison-Escape/Assets/Scripts/User-Related/PlayerMovement.cs
+++ Prison-Escape/Assets/Scripts/User-Related/PlayerMovement.cs
@@ -6,6 +6,9 @@ public class PlayerMovement : MonoBehaviour
 {
 
 	public float speed;
+	public float sprintSpeed;
+	public float stamina;
+	private float stamina_max = 500f;
 
 	private Rigidbody2D rb;
 	private Vector2 moveVelocity;
@@ -22,6 +25,37 @@ public class PlayerMovement : MonoBehaviour
 
 		Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 		moveVelocity = input * speed;
+
+		if(Input.GetKeyDown(KeyCode.LeftShift))
+		{
+
+			speed = sprintSpeed;
+			stamina--;
+
+		}
+
+		if(Input.GetKeyUp(KeyCode.LeftShift))
+		{
+
+			speed = 10f;
+			stamina++;
+
+		}
+
+		if(stamina <= 0)
+		{
+
+			speed = 10f;
+			stamina++;
+
+		}
+
+		if(stamina > stamina_max)
+		{
+
+			stamina = stamina_max;
+
+		}
 
 	}
 
